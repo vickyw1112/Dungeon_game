deb0a82e250eec67cb2452f95f658e63a84e0502
Haibo Wang <haibo.wang2@student.unsw.edu.au>
2018-09-10 01:28:45 +1000

    write a Point class for map and create Sword class

---
+++ src/GameEngine/Sword.java
@@ -0,0 +1,18 @@
+package GameEngine;
+
+public class Sword extends GameObject implements Collectable {
+
+	public Sword(Point location) {
+		super(location);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	@Override
+	public void getCollected(GameEngine engine, Inventory playerInventory) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

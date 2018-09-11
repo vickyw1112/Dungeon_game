589468956719ae2ae6a7a6aefd36ba77cdb076c9
Kevin <z3461590@student.unsw.edu.au>
2018-09-12 00:15:05 +1000

    added exit class

--- src/GameEngine/Wall.java
+++ src/GameEngine/Wall.java
@@ -17,13 +17,13 @@ public class Wall extends GameObject  {
                     CollisionResult res = new CollisionResult(0);
                     Wall wall = (Wall)(obj1 instanceof Wall ? obj1: obj2);
                     Arrow arrow = (Arrow)(obj1 instanceof Arrow ? obj1 : obj2);
-                	// arrow is the only thin;that does soemthign different with a wall. 
+                	// arrow is the only thin;that does soemthign different with a wall.
+                    
                     if (obj2 instanceof Arrow) {
-                        
-                		res.addFlag(CollisionResult.DELETE_SECOND);
+                		res.addFlag(CollisionResult.DELETE_FIRST);
                 		return res;
                 	} else {
-                  		res.addFlag(CollisionResult.DELETE_FIRST);
+                  		res.addFlag(CollisionResult.DELETE_SECOND);
                 		return res;
                 	}
                 });
@@ -37,13 +37,15 @@ public class Wall extends GameObject  {
                 });
         
         
-        // Handler for wall and Boulder (since this is the same as player i should consider a generic here as well
+        // Handler
         gameEngine.registerCollisionHandler(new CollisionEntities(this.getClass(), Boulder.class),
                 (GameEngine engine, GameObject obj1, GameObject obj2) -> {
                     CollisionResult res = new CollisionResult(0);
                     res.addFlag(CollisionResult.REJECT);
                     return res;
-                });    
+                });           
+            
         }
 }
-
+	
+	

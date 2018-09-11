4cc660568f2e5f67bb10043147f34f572238aa06
Elliott Sun <elly.here@gmail.com>
2018-09-11 17:09:09 +1000

    Added a generic collisionhandler for all collectable objects

---
+++ src/GameEngine/CollectablesCollisionHandler.java
@@ -0,0 +1,18 @@
+
+public class CollectablesCollisionHandler implement CollisionHandler {
+	
+	
+	public int CollisionResult handle(GameEngine engine, GameObject obj1, GameObject obj2) {
+		// Have to check instance type here
+        Player player = (Player)(obj1 instanceof Player ? obj1 : obj2);
+        CollisionResult res = new CollisionResult(0);
+        if(obj2.CollectableState == 0) {
+            res.addFlag(obj1 instanceof Collectable ? CollisionResult.DELETE_FIRST : CollisionResult.DELETE_SECOND);
+            res.addFlag(CollisionResult.REFRESH_INVENTORY);
+            return res;
+        } else {
+        	res.addFlag(CollisionResult.HANDLED);
+        	return res;
+        }
+	}
+}

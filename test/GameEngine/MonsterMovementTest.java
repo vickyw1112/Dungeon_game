fd1c6fc26a46d26b124db285ea35ea1cef38c02a
Haibo Wang <haibo.wang2@student.unsw.edu.au>
2018-09-16 17:27:20 +1000

    extract a new function in map called isBlockingHere

--- test/GameEngine/MonsterMovementTest.java
+++
@@ -1,7 +0,0 @@
-package GameEngine;
-
-public class MonsterMovementTest {
-    // TODO: test diff monsters' diff strategy
-    // TODO: also test when player moves (using GameEngine#changeObjectLocation), monster will update path
-    // TODO: check all monster's method including getFacing
-}
